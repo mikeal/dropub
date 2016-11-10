bd67dd3434d360f52349c56e0fdff4db098af2eb
Mikeal Rogers <mikeal.rogers@gmail.com>
2016-11-09 22:57:02 -0800

    Transitions and instructions.

--- components/dropub-torrent.js
+++ components/dropub-torrent.js
@@ -14,7 +14,6 @@ const mojimap = {
 const startsWith = (str, x) => str.indexOf(x) === 0
 
 function fileEmoji (type) {
-  console.log(type)
   for (var key in mojimap) {
     if (startsWith(type, key)) return mojimap[key]
   }
@@ -24,11 +23,16 @@ function fileEmoji (type) {
 function fileElement (f) {
   let type = mime.lookup(f.name)
   let el = bel`
-  <div class='bb-file'>
-    ${ bel([emojione.unicodeToImage(fileEmoji(type))]) }
-    <a><span class="dropub-filename dropub-not-downloaded">${
-      f.name
-    }</span></a>
+  <div class="dropub-file">
+    <div class="dropub-file-emoji">
+      ${ bel([emojione.unicodeToImage(fileEmoji(type))]) }
+    </div>
+    <div class="dropub-filename">
+      <div class="dropub-filename-label
+                  dropub-not-downloaded"
+       ><a>${ f.name }</a></div>
+      <progress value=0></progress>
+    </div>
   </div>
   `
   return el
@@ -36,49 +40,73 @@ function fileElement (f) {
 
 function init (elem, opts) {
   let webtorrent = opts.webtorrent
-  let torrentAdded = () => {
-    let hashes = webtorrent.torrents.map(t => t.infoHash)
-    for (var i = 0; i < hashes.length; i++) {
-      if (hashes[i] === opts.torrent.infoHash) return true
-    }
-    return false
-  }
+  let torrent = opts.torrent
 
-  let startDownload = () => {
-    if (!torrentAdded()) {
-      webtorrent.add(opts.torrent.magnetURI, torrent => {
-        let _finished = () => {}
+  let filemap = {}
+  torrent.files.forEach(f => { filemap[f.name] = f })
+  let selector = 'div.dropub-file div.dropub-filename-label'
+  ;[...elem.querySelectorAll(selector)].forEach(el => {
+    let name = el.textContent.trim()
 
-        let _update = () => {
-          let downloaded = torrent.downloaded
-          let percentage = Math.round(downloaded / torrent.length * 100)
-          let val = `( ${percentage}% )`
-          elem.querySelector('span.dropub-download-radio').textContent = val
-          if (downloaded !== torrent.length) setTimeout(_update, 1000)
-          else _finished()
-        }
-        _update()
-
-        let filemap = {}
-        torrent.files.forEach(f => { filemap[f.name] = f })
-        let selector = 'div.dropub-file span.dropub-filename'
-        ;[...elem.querySelectorAll(selector)].forEach(el => {
-          let name = el.textContent.trim()
-          if (filemap[name]) {
-            filemap[name].getBlobURL((err, url) => {
-              if (err) return console.error(err)
-              elementClass(el).remove('dropub-not-downloaded')
-              el.parentNode.href = url
-              el.parentNode.download = name
-            })
-          }
-        })
+    if (filemap[name]) {
+      filemap[name].progress = el.parentNode.querySelector('progress')
+      filemap[name].getBlobURL((err, url) => {
+        if (err) return console.error(err)
+        filemap[name].progress.value = 1
+        elementClass(el).remove('dropub-not-downloaded')
+        let a = el.querySelector('a')
+        a.href = url
+        a.download = name
       })
     }
+  })
+
+  selector = 'div.dropub-download-button img'
+  ;[...elem.querySelectorAll(selector)].forEach(el => {
+    el.onclick = () => {
+      let selector = 'div.dropub-filename-label'
+      let label = el.parentNode.parentNode.querySelector(selector)
+      let name = label.textContent.trim()
+
+      console.log(filemap[name])
+      if (filemap[name]) {
+        console.log('select')
+        filemap[name].select()
+      } else {
+        console.error('Could not find file in torrent.')
+      }
+    }
+  })
+
+  torrent.on('download', () => {
+    torrent.files.forEach(f => {
+      if (f.done) return
+      let downloaded = f.downloaded
+      if (downloaded === 0) return
+      let percent = downloaded / f.length
+      f.progress.value = percent
+    })
+  })
+
+  let startDownload = () => {
+    torrent.select(0, torrent.pieces.length - 1, 1)
+
+    let _finished = () => {}
+
+    let _update = () => {
+      let downloaded = torrent.downloaded
+      let percentage = Math.round(downloaded / torrent.length * 100)
+      let val = `( ${percentage}% )`
+      elem.querySelector('span.dropub-download-radio').textContent = val
+      if (downloaded !== torrent.length) setTimeout(_update, 1000)
+      else _finished()
+    }
+    _update()
+
     elem.querySelector('span.dropub-download').style.display = 'none'
     // TODO: change to pause button
   }
-  elem.querySelector('span.dropub-download').onclick = startDownload
+  // elem.querySelector('span.dropub-download').onclick = startDownload
 }
 
 const downarrow = () => bel([emojione.toImage(':arrow_down:')])
@@ -86,20 +114,22 @@ const downarrow = () => bel([emojione.toImage(':arrow_down:')])
 const view = funky`
 ${init}
 <dropub-torrent>
+  <link href='//fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css' />
   <style>
     dropub-torrent {
+      font-family: 'Lato', sans-serif;
       background-color: #fff;
-      border: 1px solid #e1e8ed;
+      // border: 1px solid #e1e8ed;
       border-radius: 5px;
       padding: 15px;
       display: block;
       margin-bottom: 10px;
       margin-left: 10px;
       margin-right: 10px;
-      font-size: 15px;
+      font-size: 18px;
       min-height: min-content;
     }
-    span.dropub-download {
+    div.dropub-download-button img {
       cursor: pointer;
     }
     div.dropub-file {
@@ -108,12 +138,39 @@ ${init}
     div.dropub-file a {
       color: black;
     }
-    div.dropub-file span.dropub-not-downloaded {
+    div.dropub-not-downloaded a {
       color: grey;
     }
+    div.dropub-files img.emojione,
+    div.drop-toplevel-control img.emojione {
+      max-height: 1.1em;
+      font-size: 18px;
+      margin-bottom: -2px;
+      line-height: 16px;
+    }
+    div.dropub-file {
+      display: flex;
+    }
+    div.dropub-file * {
+      padding: 2px;
+    }
+    div.dropub-filename progress {
+      width: 100%;
+    }
+    div.dropub-filename-label {
+      margin-bottom: 0px;
+      padding-bottom: 0px;
+    }
+    div.dropub-filename-label a {
+      margin-bottom: 0px;
+      padding-bottom: 0px;
+    }
+    div.dropub-filename progress {
+      margin-top: 0px;
+      padding-top: 0px;
+    }
   </style>
-  <span class="dropub-download">${ downarrow() }</span>
-  <span class="dropub-download-radio"></span>
+
   <div class="dropub-files">
   ${ opts => opts.torrent.files.map(f => {
     return fileElement(f)
@@ -123,3 +180,9 @@ ${init}
 `
 
 module.exports = view
+
+  // <div class="drop-toplevel-control">
+  //   <span class="dropub-download">${ downarrow() }</span>
+  //   <span class="dropub-download-radio"></span>
+  //   <span>All</div>
+  // </div>
--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 /* globals history */
 const torrentView = require('./components/dropub-torrent')
-const webtorrent = require('webtorrent')()
+const webtorrent = require('../webtorrent')()
 const dragDrop = require('drag-drop')
 const bel = require('bel')
 const title = 'Dropub'
 
 const search = window.location.search
 const container = document.getElementById('main-container')
-const clear = () => { container.innerHTML = '' }
+const clear = () => {
+  pageTitle.innerHTML = ''
+  container.innerHTML = ''
+}
 const fill = elem => {
   clear()
   container.appendChild(elem)
@@ -18,28 +21,70 @@ const updateUrl = search => {
 
 function showTorrentView (search) {
   let magnet = `magnet:${search}`
-  console.log(magnet)
   webtorrent.add(magnet, torrent => {
-    console.log(torrent)
-    torrent.deselect(0, torrent.pieces.length - 1, false)
+    // torrent.files.forEach(f => f.deselect())
     let elem = torrentView({webtorrent, torrent})
     fill(elem)
   })
 }
 
+const dropElement = bel`
+<div class="drop-files">
+  <div class="spinner">
+    <div class="double-bounce1"></div>
+    <div class="double-bounce2"></div>
+  </div>
+</div>
+`
+dropElement.onclick = () => {
+  let input = document.getElementById('fileInput')
+  input.click()
+}
+
+let fileInput = document.getElementById('fileInput')
+
+fileInput.onchange = () => {
+  seed(fileInput.files)
+}
+
+let pageTitle = document.getElementById('page-title')
+
+function seed (files) {
+  webtorrent.seed(files, torrent => {
+    let elem = torrentView({webtorrent, torrent})
+    let search = torrent.magnetURI.slice('magnet:'.length)
+    fill(elem)
+    updateUrl(search)
+    pageTitle.innerHTML = ''
+  })
+}
+
+const processingElement = bel`
+<div class="drop-files">
+  <div class="sk-fading-circle">
+    <div class="sk-circle1 sk-circle"></div>
+    <div class="sk-circle2 sk-circle"></div>
+    <div class="sk-circle3 sk-circle"></div>
+    <div class="sk-circle4 sk-circle"></div>
+    <div class="sk-circle5 sk-circle"></div>
+    <div class="sk-circle6 sk-circle"></div>
+    <div class="sk-circle7 sk-circle"></div>
+    <div class="sk-circle8 sk-circle"></div>
+    <div class="sk-circle9 sk-circle"></div>
+    <div class="sk-circle10 sk-circle"></div>
+    <div class="sk-circle11 sk-circle"></div>
+    <div class="sk-circle12 sk-circle"></div>
+  </div>
+</div>
+`
+
 function showDropView () {
-  fill(bel`<div class="drop-files"><div>Drop files here.</div></div>`)
+  fill(dropElement)
   window.onload = () => {
     dragDrop('body', files => {
-      console.log(files)
-      webtorrent.seed(files, torrent => {
-        console.log(torrent)
-        let elem = torrentView({webtorrent, torrent})
-        let search = torrent.magnetURI.slice('magnet:'.length)
-        fill(elem)
-        console.log(search)
-        updateUrl(search)
-      })
+      fill(processingElement)
+      pageTitle.textContent = 'Processing Into a Torrent'
+      seed(files)
     })
   }
 }
@@ -48,4 +93,8 @@ if (search.length) {
   showTorrentView(search)
 } else {
   showDropView()
+}
+
+window.onpopstate = () => {
+  window.location.reload(false)
 }
