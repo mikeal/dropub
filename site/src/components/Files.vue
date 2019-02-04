0e102d177a145b36c0e8ef12864e3a0785c81561
Mikeal Rogers <mikeal.rogers@gmail.com>
2019-02-04 00:39:45 +0000

    fix: file downloads

--- site/src/components/Files.vue
+++ site/src/components/Files.vue
@@ -2,14 +2,19 @@
   <div class="files-page">
     <h2>Files</h2>
     <div>CID: {{cid}}</div>
-    <div>blah</div>
     <div v-if="files.length">
       <div v-for="file in files" v-bind:key=file.name>
-        <div>t: {{file.name}}</div>
         <div v-if="file.type == 'dir'">
           <router-link :to="{path: file.name}" append>{{file.name}}</router-link>
         </div>
         <div v-if="file.type == 'file'">
+          <div v-if="file.url">
+            <a :href=file.url>{{file.name}}</a>
+          </div>
+          <div v-else>
+            <div>{{file.name}}</div>
+            <progress :max=file.size :value=file.downloaded />
+          </div>
         </div>
       </div>
     </div>
@@ -26,19 +31,52 @@ const ipfs = new IPFS({
 })
 ipfs.ready = new Promise(resolve => ipfs.on('ready', resolve))
 
+const getReadableStream = path => new Promise((resolve, reject) => {
+  let stream = ipfs.getReadableStream(path)
+  stream.once('data', entry => resolve(entry.content))
+  stream.on('error', reject)
+  stream.on('end', () => reject(new Error('no found')))
+})
+
+const loadFiles = async (files, path) => {
+  await ipfs.ready
+  let dir = await ipfs.ls(path)
+  for (let file of dir) {
+    file.downloaded = 0
+    file.url = null
+    files.push(file)
+    if (file.type === 'file') {
+      getReadableStream(`${path}/${file.name}`).then(stream => {
+        let buffers = []
+        stream.on('data', buffer => {
+          buffers.push(buffer)
+          file.downloaded += buffer.length
+        })
+        stream.on('end', () => {
+          let f = new window.Blob(buffers, 'octet/stream')
+          file.url = URL.getObjectURL(f)
+        })
+      })
+    }
+  }
+}
+
 export default {
   name: 'files-page',
   props: ['cid'],
   data: () => ({
     files: []
   }),
-  beforeMount: async function () {
+  beforeMount: function () {
     let cid = this.$props.cid
-    await ipfs.ready
-    console.log('ready')
-    let _files = await ipfs.ls(cid)
-    for (let file of _files) {
-      this.$data.files.push(file)
+    this.$data._cid = cid
+    loadFiles(this.$data.files, cid)
+  },
+  beforeUpdate: function () {
+    if (this.$data._cid !== this.$props.cid) {
+      this.$data._cid = this.$props.cid
+      this.$data.files = []
+      loadFiles(this.$data.files, this.$props.cid)
     }
   }
 }
--- site/src/main.js
+++ site/src/main.js
@@ -16,7 +16,7 @@ const routes = [
 // 3. Create the router instance and pass the `routes` option
 // You can pass in additional options here, but let's
 // keep it simple for now.
-const router = new VueRouter({ routes })
+const router = new VueRouter({ routes, mode: 'history' })
 
 new Vue({
   render: h => h(App),
